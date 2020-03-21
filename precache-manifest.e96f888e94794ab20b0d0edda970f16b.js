567120f1e42766d5b5f2728ffbd28e6d2cb91f65
Sergey <54347464+coder288@users.noreply.github.com>
2020-03-21 23:55:01 +0300

    Add files via upload

---
+++ precache-manifest.e96f888e94794ab20b0d0edda970f16b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a318511a7792579509e5d2839e1ef5",
+    "url": "/todopwa.github.io/index.html"
+  },
+  {
+    "revision": "d9be5e26b6d61487d573",
+    "url": "/todopwa.github.io/static/css/main.f4cabfc1.chunk.css"
+  },
+  {
+    "revision": "01570d50b2da7d632bfd",
+    "url": "/todopwa.github.io/static/js/2.6cdf745f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todopwa.github.io/static/js/2.6cdf745f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9be5e26b6d61487d573",
+    "url": "/todopwa.github.io/static/js/main.d772aa7d.chunk.js"
+  },
+  {
+    "revision": "ab3c7994a15e9be8233c",
+    "url": "/todopwa.github.io/static/js/runtime-main.dbdb4e56.js"
+  },
+  {
+    "revision": "1a4496bbdd58a0f57d95413230bf0e50",
+    "url": "/todopwa.github.io/static/media/tick.1a4496bb.svg"
+  },
+  {
+    "revision": "f71ec72ee00e1bdfed3e6de1d3aa12d5",
+    "url": "/todopwa.github.io/static/media/trash.f71ec72e.svg"
+  }
+]);
